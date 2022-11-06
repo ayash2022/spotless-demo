eb6cd85f5e8a0db7c68b64e86125a52dd2a601ef
ayash2022 <ayashkant.baral@maersk.com>
2022-11-06 12:41:24 +0530

    Initial Commit

---
+++ src/main/java/com/example/spotlessdemo/SpotlessDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotlessdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotlessDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotlessDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spotlessdemo/SpotlessDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotlessdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotlessDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
